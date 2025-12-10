e6001741901f1c80d1e42fab47bfff7aaa2ce9e4
SHAHZAIB SHAHBAZ <shonamian674@gmail.com>
2025-12-10 14:19:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Ensure Typed.js is properly initialized
+var typed = new Typed('.multiple-text', {
+    strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true,
+});
